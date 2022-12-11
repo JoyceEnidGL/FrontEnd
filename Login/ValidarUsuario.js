c800fc3c0a9df9031afe9107c7247f53cd29ec5e
Joyce <jegalo2001@gmail.com>
2022-12-11 13:49:56 -0600

    validacion de usuario login

---
+++ Login/ValidarUsuario.js
@@ -0,0 +1,21 @@
+var attempt = 3;
+function validate(){
+    var nombre = document.getElementById("nombre").value;
+    var contrasena = document.getElementById("contrasena").value;
+    if(nombre == "admin" && contrasena == "123456"){
+        window.location = "../index.html";
+        return false;
+    }if(nombre == "cliente" && contrasena == "654321"){
+        alert("Ingreso exitosamente");
+        window.location = "../index2.html";
+        return false;
+    }else{
+        attempt--;
+    }
+    alert("Te queda" + attempt + " intentos mas")
+    if(attempt == 0){
+        document.getElementById("nombre").disable = true;
+        document.getElementById("contrasena").disable = true;
+        document.getElementById("ingresar").disable = true;
+    }
+}
